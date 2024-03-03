1ef48c1db73a9e2eb2ea0db7c3ae3910420ee37c
Ronan Balusson <75973102+HappyChouh@users.noreply.github.com>
2024-03-03 17:50:10 +0100

    Update dbConnection.ts

--- db/dbConnection.ts
+++ db/dbConnection.ts
@@ -3,7 +3,7 @@ import { Pool } from "pg";
 export const pool = new Pool({
   user: "admin",
   host: "cl845053-001.eu.clouddb.ovh.net",
-  database: "android-game-db",
+  database: "la_horde_2",
   password: "TGw4yPrG",
   port: 35954,
 });
